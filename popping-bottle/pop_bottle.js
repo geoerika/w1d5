ecb94b6479da9c7e88d0aeaefec5233e2554841a
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-26 03:20:22 +0000

    displayed detailed results

--- popping-bottle/pop_bottle.js
+++ popping-bottle/pop_bottle.js
@@ -1,39 +1,46 @@
-const amount = process.argv[2];
-console.log(amount);
+const amount = parseInt(process.argv[2]);
 
-const purchasePop = (amount) => {
+const purchasePop = (amount) => { //returns nr of pop bottles bought with an amount of money or empty bottles
   return Math.floor(amount / 2);
 }
 
-const capPop = (amount) => {
+const capPop = (amount) => { //returns nr of pop bottles bought with empty caps
   return Math.floor(amount / 4);
 }
 
+let result = {};
+result.total = 0;
+result.recycled = 0;
+result.caps = 0;
+
 const ultPop = (amount, emptyBottles, caps) => {
   if ((amount + emptyBottles < 2) && (amount + caps < 4 )) {
-    return 0;
+    return result;
   }
 
-  let popBottles = purchasePop(amount + emptyBottles) + capPop(amount + caps);
+  let int = purchasePop(amount + emptyBottles) + capPop(amount + caps);
+  result.total = result.total + int;
+  result.recycled = result.recycled + purchasePop(amount + emptyBottles);
+  result.caps = result.caps + capPop(amount + caps);
   emptyBottles = (amount + emptyBottles) % 2;
   caps = (amount + caps) % 4;
-  amount = popBottles;
-  popBottles = popBottles + ultPop(amount, emptyBottles, caps );
+  amount = int;
+  ultPop(amount, emptyBottles, caps);
 
-  return popBottles;
+  return result;
 }
+
 
 const totalPop = (amount) => {
 
   let total = purchasePop(amount);
-  let intPop = ultPop(total, 0, 0);
-  return total + intPop;
+  result = ultPop(total, 0, 0);
+  result.total = total + result.total;
+  return result;
 }
 
-// console.log(totalPop(0));
-// console.log(totalPop(1));
-// console.log(totalPop(2));
-// console.log(totalPop(4));
-// console.log(totalPop(6));
-// console.log(totalPop(10));
-console.log("number of ultimate pop bottles bought with ", amount, "dollars: ", totalPop(amount));
+totalPop(amount);
+
+console.log('TOTAL BOTTLES: ', result.total,'\n','TOTAL EARNED: ' + '\n',
+            '  BOTTLES: ' + result.recycled + '\n',
+            '  CAPS: ', result.caps);
